2814cb399538618340148efa196d5b3a81299dc0
Romain-Gyt <Romain-Gyt@users.noreply.github.com>
2025-03-28 13:38:15 +0000

    deploy: f3f5244408c8149c3a037218ec7e71062b27174b

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-const { log } = require("console");
 
-log('Oui ca marche');
+
+console.log('Oui ca marche');
+
+const url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=40";
+
+fetch(url)
+  .then(response => response.json()) // Correction de "resoponse"
+  .then(pokemon => {
+    console.log(pokemon); // Correction de "log" => "console.log"
+  })
+  .catch(error => console.error("Erreur lors de la récupération des Pokémon :", error));
